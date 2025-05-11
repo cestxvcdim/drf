b378ea85f88e3053040b8aa5dfa8b7ad901bae45
vvvadim <v.vladimirov@it-park.tech>
2025-05-11 12:19:16 +0300

    add little changes

--- edu/serializers.py
+++ edu/serializers.py
@@ -24,9 +24,13 @@ class CourseDetailSerializer(serializers.ModelSerializer):
     lessons = LessonSerializer(many=True, read_only=True)
     is_subscribed = SerializerMethodField(read_only=True)
 
-    @staticmethod
-    def get_is_subscribed(obj):
-        return Subscription.objects.filter(course=obj).exists()
+
+    def get_is_subscribed(self, obj):
+        request = self.context.get('request', None)
+        user = getattr(request, 'user', None)
+        if not user or not user.is_authenticated:
+            return False
+        return Subscription.objects.filter(course=obj, user=user).exists()
 
     @staticmethod
     def get_lessons_count(obj):
